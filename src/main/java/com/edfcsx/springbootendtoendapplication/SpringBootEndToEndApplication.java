d2c1c6b13bcf5271a7ab4d2ef63969e07f8f8b3e
Silva, Eduardo Felipe <eduardo.felipe.silva@accenture.com>
2021-02-01 17:47:49 -0300

    initial commit

---
+++ src/main/java/com/edfcsx/springbootendtoendapplication/SpringBootEndToEndApplication.java
@@ -0,0 +1,13 @@
+package com.edfcsx.springbootendtoendapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEndToEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEndToEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edfcsx/springbootendtoendapplication/SpringBootEndToEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edfcsx.springbootendtoendapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEndToEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
