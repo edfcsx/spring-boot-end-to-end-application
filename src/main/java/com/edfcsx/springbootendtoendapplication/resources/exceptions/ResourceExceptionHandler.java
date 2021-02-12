80550ee9bc8302b53d3696b93ca7701042c6229b
Silva, Eduardo Felipe <eduardo.felipe.silva@accenture.com>
2021-02-12 11:03:47 -0300

    created exceptions to database error and resource not found

---
+++ src/main/java/com/edfcsx/springbootendtoendapplication/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.edfcsx.springbootendtoendapplication.resources.exceptions;
+
+import com.edfcsx.springbootendtoendapplication.services.exceptions.DatabaseException;
+import com.edfcsx.springbootendtoendapplication.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
